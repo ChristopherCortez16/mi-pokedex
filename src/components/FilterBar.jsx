c2680c6f6366815a35860ca15461453126284839
ChristopherCortez16 <cortezchristopher1@gmail.com>
2023-04-10 20:37:16 -0600

    Subiendo archivo

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,166 @@
+import React, { useContext } from 'react';
+import { PokemonContext } from '../context/PokemonContext';
+
+export const FilterBar = () => {
+  const { active, hadleCheckbox } = useContext(PokemonContext);
+
+  return (
+    <div className={`container-filters ${active ? 'active' : ''} `}>
+      <div className='filter-by-type'>
+        <span>Tipo</span>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='grass'
+            id='grass'
+          />
+          <label htmlFor='grass'>Planta</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='fire'
+            id='fire'
+          />
+          <label htmlFor='fire'>Fuego</label>
+        </div>
+        <div className='group-type'>
+          <input type='checkbox' onChange={hadleCheckbox} name='bug' id='bug' />
+          <label htmlFor='bug'>Bicho</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='fairy'
+            id='fairy'
+          />
+          <label htmlFor='fairy'>Hada</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='dragon'
+            id='dragon'
+          />
+          <label htmlFor='dragon'>Dragón</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='shadow'
+            id='shadow'
+          />
+          <label htmlFor='shadow'>Fantasma</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='ground'
+            id='ground'
+          />
+          <label htmlFor='ground'>Tierra</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='normal'
+            id='normal'
+          />
+          <label htmlFor='normal'>Normal</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='psychic'
+            id='psychic'
+          />
+          <label htmlFor='psychic'>Psíquico</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='steel'
+            id='steel'
+          />
+          <label htmlFor='steel'>Acero</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='dark'
+            id='dark'
+          />
+          <label htmlFor='dark'>Siniestro</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='electric'
+            id='electric'
+          />
+          <label htmlFor='electric'>Eléctrico</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='fighting'
+            id='fighting'
+          />
+          <label htmlFor='fighting'>Lucha</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='flying'
+            id='flying'
+          />
+          <label htmlFor='flying'>Volador</label>
+        </div>
+        <div className='group-type'>
+          <input type='checkbox' onChange={hadleCheckbox} name='ice' id='ice' />
+          <label htmlFor='ice'>Hielo</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='poison'
+            id='poison'
+          />
+          <label htmlFor='poison'>Veneno</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='rock'
+            id='rock'
+          />
+          <label htmlFor='rock'>Roca</label>
+        </div>
+        <div className='group-type'>
+          <input
+            type='checkbox'
+            onChange={hadleCheckbox}
+            name='water'
+            id='water'
+          />
+          <label htmlFor='water'>Agua</label>
+        </div>
+      </div>
+    </div>
+  );
+};
